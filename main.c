15e1af83d74c7faef1efc8f10d45051dc201be57
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-21 14:59:16 +0545

    setup the general skeleton for gtk coding

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+static void activate(GtkApplication *app,gpointer user_data) {
+
 }
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+    app= gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
+}
+
