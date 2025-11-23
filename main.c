804577d8d2cc3f2f1f7d37f951a286cac7df7e39
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-23 14:55:00 +0545

    made it so that the string of the widget type name gets store in widget[widgetCount].type.widgetTypeName

--- main.c
+++ main.c
@@ -204,8 +204,10 @@ void  registerWidget(){
     widget[widgetCount].isOccupied = 1;
     strcpy(widget[widgetCount].widgetName,gtk_editable_get_text(GTK_EDITABLE(entryWidgetName)));
     const char *widgetType = gtk_string_list_get_string(GTK_STRING_LIST(listWidgets), gtk_drop_down_get_selected(GTK_DROP_DOWN(dropdownWidgets)));
-    if(strcmp(widgetType,"Button")==0){
+    strcpy(widget[widgetCount].type.widgetTypeName,widgetType);
+    if(strcmp(widget[widgetCount].type.widgetTypeName,"Button")==0){
     widget[widgetCount].widget = gtk_button_new_with_label(gtk_editable_get_text(GTK_EDITABLE(entryLabel)));
+
 }
     else if(strcmp(widgetType,"Label")==0){
         widget[widgetCount].widget = gtk_label_new(gtk_editable_get_text(GTK_EDITABLE(entryLabel)));
