64b106efd313de1a5f08309aae6dcf9eb09d6c5e
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-23 23:06:08 +0545

    finalised the template main.c file

--- ExportedGui/main.c
+++ ExportedGui/main.c
@@ -46,8 +46,42 @@ struct widgets{
     }margin;
 }widget[100];
 int widgetCount;
-void declareWidgets();
-void readWidgetData();
+void declareWidgets() {
+    //Sets the widgets as null
+    int i=0;
+    while (widget[i].widget!=NULL) {
+        widget[i].widget=NULL;
+        i++;
+    }
+    for (int j = 0; j<widgetCount;j++) {
+        if (widget[j].type.isButton==1) {
+            widget[j].widget = gtk_button_new_with_label(widget[j].button.label);
+        }else if (widget[j].type.isLabel==1){
+            widget[j].widget = gtk_label_new(widget[j].label.label);
+        }else if (widget[j].type.isTextView==1){
+            widget[j].widget = gtk_text_view_new();
+        }else if (widget[j].type.isEntry==1) {
+            widget[j].widget = gtk_entry_new();
+            if (widget[j].entry.hasPlaceHolderText==1) {
+                gtk_entry_set_placeholder_text(GTK_ENTRY(widget[j].widget),widget[j].entry.placeholderText);
+            }
+        }
+        gtk_widget_set_margin_top(widget[j].widget,widget[j].margin.top);
+        gtk_widget_set_margin_bottom(widget[j].widget,widget[j].margin.bottom);
+        gtk_widget_set_margin_start(widget[j].widget,widget[j].margin.start);
+        gtk_widget_set_margin_end(widget[j].widget,widget[j].margin.end);
+
+    }
+}
+
+void readWidgetData() {
+    FILE *file = fopen("widget.data","r");
+    fread(&widgetCount, sizeof(widgetCount), 1, file);
+    fread(&widget,sizeof(widget),widgetCount,file);
+    fclose(file);
+}
+
+void yourCode();
 
 static void activate(GtkApplication *app,gpointer user_data) {
     //Init of windowChild
@@ -83,42 +117,15 @@ static void activate(GtkApplication *app,gpointer user_data) {
                 widget[i].minSize.height);
         }
     }
+    //You can put your code here do not touch the other stuff you didn't create
+    yourCode();
 
 }
 
-void declareWidgets() {
-    //Sets the widgets as null
-    int i=0;
-    while (widget[i].widget!=NULL) {
-        widget[i].widget=NULL;
-        i++;
-    }
-    for (int j = 0; j<widgetCount;j++) {
-        if (widget[j].type.isButton==1) {
-            widget[j].widget = gtk_button_new_with_label(widget[j].button.label);
-        }else if (widget[j].type.isLabel==1){
-            widget[j].widget = gtk_label_new(widget[j].label.label);
-        }else if (widget[j].type.isTextView==1){
-            widget[j].widget = gtk_text_view_new();
-        }else if (widget[j].type.isEntry==1) {
-            widget[j].widget = gtk_entry_new();
-            if (widget[j].entry.hasPlaceHolderText==1) {
-                gtk_entry_set_placeholder_text(GTK_ENTRY(widget[j].widget),widget[j].entry.placeholderText);
-            }
-        }
-        gtk_widget_set_margin_top(widget[j].widget,widget[j].margin.top);
-        gtk_widget_set_margin_bottom(widget[j].widget,widget[j].margin.bottom);
-        gtk_widget_set_margin_start(widget[j].widget,widget[j].margin.start);
-        gtk_widget_set_margin_end(widget[j].widget,widget[j].margin.end);
+void yourCode() {
+    //Put yo code here read the documentation of the widgets
 
-    }
-}
 
-void readWidgetData() {
-    FILE *file = fopen("widget.data","r");
-    fread(&widgetCount, sizeof(widgetCount), 1, file);
-    fread(&widget,sizeof(widget),widgetCount,file);
-    fclose(file);
 }
 
 
