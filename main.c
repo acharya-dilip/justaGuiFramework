2d8e7fa0194eb70b061c7ef706cf93db4ee918a1
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-27 17:12:20 +0545

    added some spacing between the widgets that I removed being an idiot

--- main.c
+++ main.c
@@ -132,11 +132,16 @@ void screenAddWidget() {
     defaultAdjustmentMarginEnd = gtk_adjustment_new(0.0, 0.0, 1000.0, 1.0, 10.0, 0.0);
     defaultAdjustmentMarginTop = gtk_adjustment_new(0.0, 0.0, 1000.0, 1.0, 10.0, 0.0);
     defaultAdjustmentMarginBottom = gtk_adjustment_new(0.0, 0.0, 1000.0, 1.0, 10.0, 0.0);
+
+
+
     //Init of windowAddWidget
     windowAddWidget = gtk_window_new();
     gtk_window_set_default_size(GTK_WINDOW(windowAddWidget),300,300);
     gtk_window_set_title(GTK_WINDOW(windowAddWidget),"Add Widget");
     gtk_window_present(GTK_WINDOW(windowAddWidget));
+
+
     //Init of gridParentWidgets
     GtkWidget *gridParentWidgets = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(windowAddWidget),gridParentWidgets);
@@ -145,22 +150,30 @@ void screenAddWidget() {
     gtk_widget_set_margin_bottom(gridParentWidgets,10);
     gtk_widget_set_margin_end(gridParentWidgets,10);
     gtk_widget_set_margin_start(gridParentWidgets,10);
+
+
     //Init of entryWidgetName
     entryWidgetName = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(gridParentWidgets),entryWidgetName,0,0,8,1);
     gtk_entry_set_placeholder_text(GTK_ENTRY(entryWidgetName),"Enter Widget Name");
     //Margins & Paddings
     gtk_widget_set_size_request(entryWidgetName,210,-1);
+
+
     //Init of dropdownWidgets
     listWidgets = gtk_string_list_new(widgetList);
     dropdownWidgets = gtk_drop_down_new(G_LIST_MODEL (listWidgets), NULL);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),dropdownWidgets,8,0,2,1);
+
+
     //Init of entrylabel
     entryLabel = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(gridParentWidgets),entryLabel,0,1,10,1);
     gtk_entry_set_placeholder_text(GTK_ENTRY(entryLabel),"Enter Widget Label/Placeholder Text");
     //Margins & Paddings
     gtk_widget_set_margin_top(entryLabel,10);
+
+
     //Init of labelPlacement
     GtkWidget *labelPlacement = gtk_label_new("Grid Placement:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelPlacement,0,2,6,1);
@@ -168,36 +181,60 @@ void screenAddWidget() {
     gtk_widget_set_margin_start(labelPlacement,6);
     gtk_widget_set_margin_top(labelPlacement,6);
     gtk_widget_set_margin_bottom(labelPlacement,10);
+
+
+
     //Init of labelGridPlacementRow
     GtkWidget *labelGridPlacementRow = gtk_label_new("Row:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelGridPlacementRow,0,3,1,1);
     gtk_widget_set_margin_top(labelGridPlacementRow,4);
+
+
+
     //Init of spinbuttonGridPlacementRow
     spinbuttonGridPlacementRow = gtk_spin_button_new(defaultAdjustmentRow,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonGridPlacementRow,1,3,5,1);
+
+
     //Init of labelGridPlacementColumn
     GtkWidget *labelGridPlacementColumn = gtk_label_new("Col:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelGridPlacementColumn,6,3,1,1);
     gtk_widget_set_margin_top(labelGridPlacementColumn,4);
+
+
+
     //Init of spinbuttonGridPlacementColumn
     spinbuttonGridPlacementColumn = gtk_spin_button_new(defaultAdjustmentColumn ,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonGridPlacementColumn,7,3,3,1);
+
+
     //Init of labelGridPlacementRowSpan
     GtkWidget *labelGridPlacementRowSpan = gtk_label_new("Height:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelGridPlacementRowSpan,0,4,1,1);
     gtk_widget_set_margin_top(labelGridPlacementRowSpan,4);
+
+
     //Init of spinbuttonGridPlacementRowSpan
     spinbuttonGridPlacementRowSpan = gtk_spin_button_new(defaultAdjustmentRowSpan,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonGridPlacementRowSpan,1,4,5,1);
     gtk_widget_set_margin_top(spinbuttonGridPlacementRowSpan,4);
+
+
+
     //Init of labelGridPlacementColumnSpan
     GtkWidget *labelGridPlacementColumnSpan = gtk_label_new("Width:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelGridPlacementColumnSpan,6,4,1,1);
     gtk_widget_set_margin_top(labelGridPlacementColumnSpan,4);
+
+
+
     //Init of spinbuttonGridPlacementColumnSpan
     spinbuttonGridPlacementColumnSpan = gtk_spin_button_new(defaultAdjustmentColumnSpan ,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonGridPlacementColumnSpan,7,4,3,1);
     gtk_widget_set_margin_top(spinbuttonGridPlacementColumnSpan,4);
+
+
+
     //Init of labelMinSize
     GtkWidget *labelMinSize = gtk_label_new("Minimum Size:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMinSize,0,5,6,1);
@@ -205,22 +242,34 @@ void screenAddWidget() {
     gtk_widget_set_margin_start(labelMinSize,6);
     gtk_widget_set_margin_top(labelMinSize,6);
     gtk_widget_set_margin_bottom(labelMinSize,10);
+
+
+
     //Init of labelMinHeight
     GtkWidget *labelMinHeight = gtk_label_new("Height:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMinHeight,0,6,1,1);
     gtk_widget_set_margin_top(labelMinHeight,4);
+
+
+
     //Init of spinbuttonMinHeight
     spinbuttonMinHeight = gtk_spin_button_new(defaultAdjustmentMinHeight,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMinHeight,1,6,5,1);
     gtk_widget_set_margin_top(spinbuttonMinHeight,4);
+
+
     //Init of labelMinWidth
     GtkWidget *labelMinWidth = gtk_label_new("Width:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMinWidth,6,6,1,1);
     gtk_widget_set_margin_top(labelMinWidth,4);
+
+
     //Init of spinbuttonMinWidth
     spinbuttonMinWidth = gtk_spin_button_new(defaultAdjustmentMinWidth ,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMinWidth,7,6,3,1);
     gtk_widget_set_margin_top(spinbuttonMinWidth,4);
+
+
     //Init of labelMargins
     GtkWidget *labelMargins = gtk_label_new("Margin Size for Widget:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMargins,0,7,10,1);
@@ -228,45 +277,83 @@ void screenAddWidget() {
     gtk_widget_set_margin_start(labelMargins,6);
     gtk_widget_set_margin_top(labelMargins,10);
     gtk_widget_set_margin_bottom(labelMargins,10);
+
+
+
     //Init of labelMarginStart
     GtkWidget *labelMarginStart = gtk_label_new("Start:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMarginStart,0,8,1,1);
     gtk_widget_set_margin_top(labelMarginStart,4);
+
+
+
     //Init of spinbuttonMarginStart
     spinbuttonMarginStart = gtk_spin_button_new(defaultAdjustmentMarginStart,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMarginStart,1,8,5,1);
     gtk_widget_set_margin_top(spinbuttonMarginStart,4);
+
+
+
     //Init of labelMarginEnd
     GtkWidget *labelMarginEnd = gtk_label_new("End:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMarginEnd,6,8,1,1);
     gtk_widget_set_margin_top(labelMarginEnd,4);
+
+
+
     //Init of spinbuttonMarginEnd
     spinbuttonMarginEnd = gtk_spin_button_new(defaultAdjustmentMarginEnd ,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMarginEnd,7,8,3,1);
     gtk_widget_set_margin_top(spinbuttonMarginEnd,4);
+
+
+
     //Init of labelMarginTop
     GtkWidget *labelMarginTop = gtk_label_new("Top:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMarginTop,0,9,1,1);
     gtk_widget_set_margin_top(labelMarginTop,4);
+
+
+
     //Init of spinbuttonMarginTop
     spinbuttonMarginTop = gtk_spin_button_new(defaultAdjustmentMarginTop,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMarginTop,1,9,5,1);
     gtk_widget_set_margin_top(spinbuttonMarginTop,4);
+
+
+
     //Init of labelMarginBottom
     GtkWidget *labelMarginBottom = gtk_label_new("Bottom:");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),labelMarginBottom,6,9,1,1);
     gtk_widget_set_margin_top(labelMarginBottom,4);
+
+
+
     //Init of spinbuttonMarginBottom
     spinbuttonMarginBottom = gtk_spin_button_new(defaultAdjustmentMarginBottom ,1,0);
     gtk_grid_attach(GTK_GRID(gridParentWidgets),spinbuttonMarginBottom,7,9,3,1);
     gtk_widget_set_margin_top(spinbuttonMarginBottom,4);
+
+
+
     //Init of buttonAddWidget
     GtkWidget *buttonRegisterWidget = gtk_button_new_with_label("Add Widget");
     gtk_grid_attach(GTK_GRID(gridParentWidgets),buttonRegisterWidget,0,20,10,1);
     g_signal_connect(buttonRegisterWidget,"clicked",G_CALLBACK(registerWidget),NULL);
     //Margins & Paddings
     gtk_widget_set_margin_top(buttonRegisterWidget,10);
+
+
+
+
+
 }
+
+
+
+
+
+
 //It registers/Inputs the values of the widgets and their properties in the widget struct
 void  registerWidget() {
     //Function to hold all the logic for when the buttonRegister
