ee907c20bf1a703200691108e8ea050082d7c082
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-30 16:44:50 +0545

    removed the entire thing with the buttoninfo and screeninfo

--- main.c
+++ main.c
@@ -5,7 +5,6 @@ struct widgets{
     char widgetName[100];
     GtkWidget *widget;
     //For the display thing
-    GtkWidget *buttonInfo;
     GtkWidget *placeholderLabel;
     GtkWidget *gridChildBox;
     GtkWidget *labelWidgetInfo;
@@ -52,7 +51,6 @@ void exportGui();
 void declareWidgets();
 void widgetAttributeWidgets(GtkWidget *window, int i);
 void editWidget(GtkButton *button, gpointer user_data);
-void screenWidgetInfo(GtkButton *button, gpointer user_data);
 
 
 
@@ -526,12 +524,6 @@ void  registerWidget(GtkButton *button, gpointer user_data) {
                 widget[i].labelWidgetInfo = gtk_label_new(widgetInfo);
                 gtk_grid_attach(GTK_GRID(widget[i].gridChildBox),widget[i].labelWidgetInfo,1,0,1,1);
 
-                //Init of buttonInfo
-                widget[i].buttonInfo = gtk_button_new_with_label("ℹ️");
-                g_signal_connect(widget[i].buttonInfo,"clicked",G_CALLBACK(screenWidgetInfo),GINT_TO_POINTER(i));
-                gtk_grid_attach(GTK_GRID(widget[i].gridChildBox),widget[i].buttonInfo,0,0,1,1);
-                gtk_widget_set_valign(widget[i].buttonInfo,GTK_ALIGN_CENTER);
-                gtk_widget_set_halign(widget[i].buttonInfo,GTK_ALIGN_CENTER);
 
                 //Init of buttonEditWidget
                 widget[i].buttonEditWidget = gtk_button_new_with_label("📝");
@@ -555,19 +547,6 @@ void  registerWidget(GtkButton *button, gpointer user_data) {
     }
 
 
-    void screenWidgetInfo(GtkButton *button, gpointer user_data) {
-        int i = GPOINTER_TO_INT(user_data);
-
-
-        //init of windowWidgetInfo
-        GtkWidget *windowWidgetInfo = gtk_window_new();
-        gtk_window_set_default_size(GTK_WINDOW(windowWidgetInfo),350,400);
-        gtk_window_set_title(GTK_WINDOW(windowWidgetInfo),"Widget Info");
-        gtk_window_present(GTK_WINDOW(windowWidgetInfo));
-
-
-
-    }
 
 
 
